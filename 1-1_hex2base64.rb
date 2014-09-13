3c17f4a23d1f3033308d1f7dcf676ccdf1ed3ef5
Sagar Jauhari <sjauhari@panoramaed.com>
2014-09-13 14:45:32 -0400

    added 1-2

--- 1-1_hex2base64.rb
+++ 1-1_hex2base64.rb
@@ -1,19 +1,15 @@
 def hex_to_base64(str)
   new_str = ""
   while str.length > 0
-    a = hex_to_dec(str[0])
-    b = hex_to_dec(str[1])
-    c = hex_to_dec(str[2])
+    a = str[0].to_i 16
+    b = str[1].to_i 16
+    c = str[2].to_i 16
 
     new_str << dec_to_base_64((a << 2) + (b >> 2)).to_s
     new_str << dec_to_base_64(((b&3) << 4) + c).to_s
     str = str[3, str.length]
   end
   new_str
-end
-
-def hex_to_dec(chr)
-  chr.to_i 16
 end
 
 def dec_to_base_64(num)
