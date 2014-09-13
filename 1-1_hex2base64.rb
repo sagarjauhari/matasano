fd5978a5e2a2e3800f44996231730d1bdd2e9118
Sagar Jauhari <sjauhari@panoramaed.com>
2014-09-13 14:34:17 -0400

    using to_i(16)

--- 1-1_hex2base64.rb
+++ 1-1_hex2base64.rb
@@ -6,22 +6,14 @@ def hex_to_base64(str)
     c = hex_to_dec(str[2])
 
     new_str << dec_to_base_64((a << 2) + (b >> 2)).to_s
-    new_str << dec_to_base_64(((b&3) << 4) + c).to_s 
+    new_str << dec_to_base_64(((b&3) << 4) + c).to_s
     str = str[3, str.length]
   end
   new_str
 end
 
-def hex_to_dec(num)
-  if num.ord >= '0'.ord && num.ord <= '9'.ord
-    num.to_i
-  elsif num.ord >= 'A'.ord && num.ord <= 'F'.ord
-    num.ord - 'A'.ord + 10
-  elsif num.ord >= 'a'.ord && num.ord <= 'f'.ord
-    num.ord - 'a'.ord + 10
-  else
-    raise 'Number #{num} cannot be convered to dec'
-  end
+def hex_to_dec(chr)
+  chr.to_i 16
 end
 
 def dec_to_base_64(num)
