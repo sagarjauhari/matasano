ed2017851b2c2d4d0e9c48ab475de3757d39515d
Sagar Jauhari <sagarjauhari@gmail.com>
2015-10-10 21:17:34 -0400

    Adding correct hamming dist method

--- helpers.rb
+++ helpers.rb
@@ -12,3 +12,24 @@ end
 def int_arr_to_str(array)
   array.pack('C*')
 end
+
+# Counts the number of '1's in the binary format of an integer
+def num_one_bits(number)
+  number.to_s(2).split("").reduce(0){ |sum, n| sum + n.to_i }
+end
+
+def hamming_dist(str1, str2)
+  unless str1.length == str2.length
+    raise "Strings of different length not supported"
+  end
+
+  len = str1.length
+  (0..len-1).map do |i|
+    num_one_bits(str1[i].ord ^ str2[i].ord)
+  end.reduce(&:+)
+end
+
+# Tests
+unless hamming_dist("this is a test", "wokka wokka!!!") == 37
+  raise "Hamming distance incorrect"
+end
