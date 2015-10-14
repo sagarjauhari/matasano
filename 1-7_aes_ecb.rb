86fe0c9c3e028a9d86d29b9d278ac0629496a0ca
Sagar Jauhari <sagarjauhari@gmail.com>
2015-10-13 23:39:21 -0400

    restructured code, converted to class

--- 1-7_aes_ecb.rb
+++ 1-7_aes_ecb.rb
@@ -1,75 +1,88 @@
 require "./helpers.rb"
 
-n_cycles = 10 # Because key is 16 bytes long
-state = Matrix[]    # State global var
+class AES
+  def initialize
+    @n_cycles = 10 # Because key is 16 bytes long
+    @state = Matrix[]    # State global var
+  end
 
-# AES Round 1/4
-def sub_bytes
-end
+  def aes_ecb_encrypt(filename, key)
+    key_arr = key.split("")
 
-# AES inv Round 4/4
-def sub_bytes_inv
-end
+    File.open(filename, "r") do |file|
+      data = pad_data(file.read, 16)[0..127]
+      data.unpack("C*").each_slice(16).map do |slice|
+        @state = array_to_matrix(slice)
 
-# AES Round 2/4
-def shift_rows
-end
+        aes_encrypt_block
 
-# AES inv Round 3/4
-def shift_rows_inv
-end
-
-# AES Round 3/4
-def mix_cols
-end
-
-# AES inv Round 2/4
-def mix_cols_inv
-end
-
-# AES Round 4/4
-def add_round_key(round_key)
-end
-
-# AES inv Round 1/4
-def add_round_key_inv(round_key)
-end
-
-def round(round_key, final: false)
-  sub_bytes
-  shift_rows
-  mix_cols unless final
-  add_round_key(round_key)
-end
-
-def round_inv(round_key, final: false)
-  add_round_key_inv(round_key)
-  mix_cols_inv unless final
-  shift_rows_inv
-  sub_bytes_inv
-end
-
-def aes_encrypt_block(data_arr, key_arr)
-  matrix = array_to_matrix(data_arr)
-  data_arr
-end
-
-def aes_decrypt_block(data_arr)
-end
-
-
-def aes_ecb_encrypt(filename, key)
-  key_arr = key.split("")
-
-  File.open(filename, "r") do |file|
-    data = pad_data(file.read, 16)
-    data.unpack("C*").each_slice(16).map do |slice|
-      ap aes_encrypt_block(slice, key_arr).pack("C*")
+        ap matrix_to_array(@state).pack("C*")
+      end
     end
+  end
+
+  def aes_ecb_decrypt(filename, key)
+  end
+
+  private
+
+  def aes_encrypt_block
+    round("HELLO")
+  end
+
+  def round(round_key, final: false)
+    sub_bytes
+    shift_rows
+    mix_cols unless final
+    add_round_key(round_key)
+  end
+
+  # AES Round 1/4
+  def sub_bytes
+    puts "# Sub Bytes"
+    ap @state
+  end
+
+  # AES inv Round 4/4
+  def sub_bytes_inv
+  end
+
+  # AES Round 2/4
+  def shift_rows
+  end
+
+  # AES inv Round 3/4
+  def shift_rows_inv
+  end
+
+  # AES Round 3/4
+  def mix_cols
+  end
+
+  # AES inv Round 2/4
+  def mix_cols_inv
+  end
+
+  # AES Round 4/4
+  def add_round_key(round_key)
+  end
+
+  # AES inv Round 1/4
+  def add_round_key_inv(round_key)
+  end
+
+  def round_inv(round_key, final: false)
+    add_round_key_inv(round_key)
+    mix_cols_inv unless final
+    shift_rows_inv
+    sub_bytes_inv
+  end
+
+  def aes_decrypt_block
   end
 end
 
-def aes_ecb_decrypt(filename, key)
-end
-
-aes_ecb_encrypt("1-7_test_plain_text.txt", "YELLOW SUBMARINE")
+AES.new.aes_ecb_encrypt(
+  "1-7_test_plain_text.txt",
+  "YELLOW SUBMARINE"
+)
